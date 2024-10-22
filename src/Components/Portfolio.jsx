e0f917c11e2571b934dced8e08f7a57fa7e0406b
Anushabore60 <boreanusha60@gmail.com>
2024-10-22 15:02:29 +0530

    initial commit

--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -31,29 +31,24 @@ const imageAltText = "desktop with books and laptop";
  */
 const projectList = [
   {
-    title: "10 Things To Know About Azure Static Web Apps 🎉",
+    title: " basic Paytm project",
     description:
-      "Collaboration to create a beginner friendly article to help explain Azure Static Web Apps and tooling to get started.",
-    url: "https://dev.to/azure/10-things-to-know-about-azure-static-web-apps-3n4i",
+      "Implimented basic paytm application using react and node.js javascript, implimented user authentication using jwt tokens used mongodb for the database and implimented transactions to rollback if the payment failed ",
+    url: "https://github.com/Anushabore60/paytmproject",
   },
   {
-    title: "Web Development for Beginners",
+    title: "Weather application using API ",
     description:
-      "Contributed sketch note imagery to accompany each lesson. These help provide visual representation of what is being taught.",
-    url: "https://github.com/microsoft/web-dev-for-beginners",
+      "developed a responsive weather application using React, Tailwindd css, API keys  and deployed this on Vercel ",
+    url: "https://weather-app-seven-puce.vercel.app/",
   },
   {
-    title: "My Resume Site",
+    title: "Figma designs ",
     description:
-      "Created from Microsoft's resume workshop and deployed to GitHub pages. Includes my experience and design abilities.",
-    url: "https://github.com/microsoft/workshop-library/tree/main/full/build-resume-website",
+      "Created Responsive designs using Figma  1.food devlivering application design 2.E-commerce design.",
+    url: "",
   },
-  {
-    title: "GitHub Codespaces and github.dev",
-    description:
-      "Video interview to explain when to use GitHub.dev versus GitHub Codespaces, and how best to use each tool.",
-    url: "https://www.youtube.com/watch?v=c3hHhRME_XI",
-  },
+
 ];
 
 const Portfolio = () => {
